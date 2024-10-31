7b01f12b65abd761e63cba6312953107e818b34f
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-31 21:05:15 +0000

    Created airport class and instantiated each airport object

--- planning.js
+++ planning.js
@@ -1,30 +1,61 @@
 const fs = require('fs');
 
 function readCsv(filename, delimiter = ',') {
-    try {
-        const fileContent = fs.readFileSync(filename, { encoding: 'utf-8' });
-        const rows = fileContent.split('\n');
-        const data = [];
+  try {
+    const fileContent = fs.readFileSync(filename, { encoding: 'utf-8' });
+    const rows = fileContent.split('\n');
+    const data = [];
 
-        for (let i = 1; i < rows.length; i++) {
-            const row = rows[i].trim();
-            if (row) {
-                const columns = row.split(delimiter);
-                data.push(columns);
-            }
-        }
-
-        return data;
-    } catch (err) {
-        console.error("Error reading file:", err.message);
-        return null;
+    for (let i = 1; i < rows.length; i++) {
+      const row = rows[i].trim();
+      if (row) {
+        const columns = row.split(delimiter);
+        data.push(columns);
+      }
     }
+
+    return data;
+  } catch (err) {
+    console.error('Error reading file:', err.message);
+    return null;
+  }
 }
 
-// Usage example
-const airportsData = readCsv('airports.csv');
-if (airportsData) {
-    airportsData.forEach(row => {
-        console.log(row);
-    });
+// Class for airports and their information
+class airport {
+  #code;
+  #name;
+  #distFromMAN;
+  #distFromLGW;
+
+  constructor([code, name, distFromMAN, distFromLGW]) {
+    this.#code = code;
+    this.#name = name;
+    this.#distFromMAN = distFromMAN;
+    this.#distFromLGW = distFromLGW;
+  }
+
+  get code() {
+    return this.#code;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get distFromMAN() {
+    return this.#distFromMAN;
+  }
+
+  get distFromLGW() {
+    return this.#distFromLGW;
+  }
 }
+
+const airportData = readCsv('airports.csv');
+
+const airportJFK = new airport(airportData[0])
+const airportORY = new airport(airportData[1])
+const airportMAD = new airport(airportData[2])
+const airportAMS = new airport(airportData[3])
+const airportCAI = new airport(airportData[4])
