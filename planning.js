12795aa22ea918877f1b10c9782350387e3e61ba
FirasLayani <firas.zahrollayani@ada.ac.uk>
2024-10-31 21:47:07 +0000

    Created aeroplane class and instantiated each aeroplane object

--- planning.js
+++ planning.js
@@ -34,28 +34,72 @@ class airport {
     this.#distFromMAN = distFromMAN;
     this.#distFromLGW = distFromLGW;
   }
-
   get code() {
     return this.#code;
   }
-
   get name() {
     return this.#name;
   }
-
   get distFromMAN() {
     return this.#distFromMAN;
   }
-
   get distFromLGW() {
     return this.#distFromLGW;
   }
 }
 
-const airportData = readCsv('airports.csv');
+// Class for aeroplanes and their information
+class aeroplane {
+  #type;
+  #costPerSeatPer100km;
+  #maxFlightRange;
+  #numEconomySeats;
+  #numBusinessSeats;
+  #numFirstClassSeats;
 
-const airportJFK = new airport(airportData[0])
-const airportORY = new airport(airportData[1])
-const airportMAD = new airport(airportData[2])
-const airportAMS = new airport(airportData[3])
-const airportCAI = new airport(airportData[4])
+  constructor([type, costPerSeatPer100km, maxFlightRange, numEconomySeats, numBusinessSeats, numFirstClassSeats]) {
+    this.#type = type;
+    this.#costPerSeatPer100km = costPerSeatPer100km;
+    this.#maxFlightRange = maxFlightRange;
+    this.#numEconomySeats = numEconomySeats;
+    this.#numBusinessSeats = numBusinessSeats;
+    this.#numFirstClassSeats = numFirstClassSeats;
+  }
+  get type() {
+    return this.#type;
+  }
+  get costPerSeatPer100km() {
+    return this.#costPerSeatPer100km;
+  }
+  get maxFlightRange() {
+    return this.#maxFlightRange;
+  }
+  get numEconomySeats() {
+    return this.#numEconomySeats;
+  }
+  get numBusinessSeats() {
+    return this.#numBusinessSeats;
+  }
+  get numFirstClassSeats() {
+    return this.#numFirstClassSeats;
+  }
+}
+
+// REFACTOR BY ITERATING THROUGH ARRAY
+// TEST EACH AEROPLANE/AIRPORT OBJECT RETURNS CORRECT ATTRIBUTES, PRIVATE TOO
+
+// Read in data
+const airportData = readCsv('airports.csv');
+const aeroplaneData = readCsv('aeroplanes.csv');
+
+// Instanstiate each airport object
+const airportJFK = new airport(airportData[0]);
+const airportORY = new airport(airportData[1]);
+const airportMAD = new airport(airportData[2]);
+const airportAMS = new airport(airportData[3]);
+const airportCAI = new airport(airportData[4]);
+
+// Instantiate each aeroplane object
+const medNarrowBody = new aeroplane(aeroplaneData[0])
+const lrgNarrowBody = new aeroplane(aeroplaneData[1])
+const medWideBody = new aeroplane(aeroplaneData[2])
